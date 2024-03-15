f63bdc34bbe45193e1ed2749add0000608854888
mathieul <mathieu.lienard@ynov.com>
2024-03-15 11:52:52 +0100

    v2

--- Script/Character.js
+++ Script/Character.js
@@ -1,47 +1,9 @@
-// class Character {
-//     constructor() {
-//         this.coordonnees = createVector(100, 100); 
-//         this.speed = 5;
-//     }
-
-//     draw() {
-//         fill(125, 145, 43); 
-//         translate(this.coordonnees.x, this.coordonnees.y)
-//         ellipse(50, 500, 30, 30);   
-//     }
-
-//     update(){
-//         let xSpeed = 0;
-//         let ySpeed = 0;
-
-//         if (keyIsDown(90)) {
-//             ySpeed = -2;
-//         }
-
-//         if (keyIsDown(83)) {
-//             ySpeed = 2;
-//         }
-
-//         if (keyIsDown(68)) {
-//             xSpeed = 2;
-//         }
-
-//         if (keyIsDown(81)) {
-//             xSpeed = -2;
-//         }
-
-//         this.coordonnees.add(xSpeed, ySpeed);
-//     }
-
-    
-// }
-
 let characterImage;
 
 class Character {
     constructor() {
         this.coordonnees = createVector(100, 100); 
-        this.speed = 5;
+        this.speed = 8;
     }
 
     draw() {
@@ -50,29 +12,43 @@ class Character {
         image(characterImage, this.coordonnees.x, this.coordonnees.y, 50, 50);
     }
 
-    update(){
+    update() {
+        console.log("coucou")
+        this.move(2)
+
+        if (mouseIsPressed && mouseButton === LEFT && this.dashCooldown <= 0) {
+            console.log("Dash bb")
+            this.move(100);
+            this.dashCooldown = 100;
+        }
+
+        if (this.dashCooldown > 0) {
+            this.dashCooldown--;
+        }
+    }
+    
+    move(speed) {
         let xSpeed = 0;
         let ySpeed = 0;
 
         if (keyIsDown(90)) {
-            ySpeed = -2;
+            ySpeed = -speed;
         }
 
         if (keyIsDown(83)) {
-            ySpeed = 2;
+            ySpeed = speed;
         }
 
         if (keyIsDown(68)) {
-            xSpeed = 2;
+            xSpeed = speed;
         }
 
         if (keyIsDown(81)) {
-            xSpeed = -2;
+            xSpeed = -speed;
         }
 
         this.coordonnees.add(xSpeed, ySpeed);
-
-    }    
+    }
 }
 
 function preload() {
