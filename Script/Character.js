4c5dbf6965a72b421a921cae52dc3bb511e010f9
mathieul <mathieu.lienard@ynov.com>
2024-03-15 10:56:07 +0100

    add images for el personage

--- Script/Character.js
+++ Script/Character.js
@@ -1,3 +1,43 @@
+// class Character {
+//     constructor() {
+//         this.coordonnees = createVector(100, 100); 
+//         this.speed = 5;
+//     }
+
+//     draw() {
+//         fill(125, 145, 43); 
+//         translate(this.coordonnees.x, this.coordonnees.y)
+//         ellipse(50, 500, 30, 30);   
+//     }
+
+//     update(){
+//         let xSpeed = 0;
+//         let ySpeed = 0;
+
+//         if (keyIsDown(90)) {
+//             ySpeed = -2;
+//         }
+
+//         if (keyIsDown(83)) {
+//             ySpeed = 2;
+//         }
+
+//         if (keyIsDown(68)) {
+//             xSpeed = 2;
+//         }
+
+//         if (keyIsDown(81)) {
+//             xSpeed = -2;
+//         }
+
+//         this.coordonnees.add(xSpeed, ySpeed);
+//     }
+
+    
+// }
+
+let characterImage;
+
 class Character {
     constructor() {
         this.coordonnees = createVector(100, 100); 
@@ -6,8 +46,8 @@ class Character {
 
     draw() {
         fill(125, 145, 43); 
-        translate(this.coordonnees.x, this.coordonnees.y)
-        ellipse(50, 500, 30, 30);   
+        //translate(this.coordonnees.x, this.coordonnees.y)
+        image(characterImage, this.coordonnees.x, this.coordonnees.y, 50, 50);
     }
 
     update(){
@@ -31,7 +71,13 @@ class Character {
         }
 
         this.coordonnees.add(xSpeed, ySpeed);
-    }
 
-    
+    }    
 }
+
+function preload() {
+    characterImage = loadImage('../img/ninja.png');
+}
+
+
+
